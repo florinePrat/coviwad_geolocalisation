0e7a8400d322b9f5eadbec7552f7ce1688aa1360
Florine <florine@matchers.fr>
2021-11-16 14:21:52 +0100

    Initial commit

---
+++ src/main/java/fr/polytech/mtp/coviwad_geolocalisation/CoviwadGeolocalisationApplication.java
@@ -0,0 +1,13 @@
+package fr.polytech.mtp.coviwad_geolocalisation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoviwadGeolocalisationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoviwadGeolocalisationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/polytech/mtp/coviwad_geolocalisation/CoviwadGeolocalisationApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.polytech.mtp.coviwad_geolocalisation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoviwadGeolocalisationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
